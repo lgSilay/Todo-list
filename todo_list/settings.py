011ee5b6b17f08a3a836a6588fc282e1001460db
Pavlo Harbuz <lgsilay1991@gmail.com>
2023-10-05 13:43:48 +0300

    Add app to install apps

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "crispy_forms",
+    # "crispy_bootstrap4"
+    "app",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
